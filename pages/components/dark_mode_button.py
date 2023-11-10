b940b39df9395d9e2cc40790d45bc951d29e73f6
Steve Jones <jonesst2608@gmail.com>
2023-11-10 20:28:41 +0000

    Fixed icon sizes

--- pages/components/dark_mode_button.py
+++ pages/components/dark_mode_button.py
@@ -6,7 +6,7 @@ from ..settings.dark_mode import dark_mode
 @component
 def DarkModeButton():
 
-    icon = Icon_Sun() if dark_mode() else Icon_Moon()
+    icon =  Icon_Moon() if dark_mode() else Icon_Sun()
 
     return html.button({'id': 'theme-toggle', 'type': 'button', 'class_name': 'rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none', 'onclick': '{toggleDarkMode}'},
         icon,
