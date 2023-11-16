b5a8e26971778d2d18bdf826fc3f99a99e2e5de9
Steve Jones <jonesst2608@gmail.com>
2023-11-16 14:21:09 +0000

    Trying to get testing to work in container

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,16 +12,24 @@ HEADLESS = False
 def anyio_backend():
     return 'asyncio'
 
+
 @pytest.fixture(scope="session", autouse=False)
 def event_loop(_request) -> Generator:  # : indirect usage
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
+
 @pytest.fixture(scope="session")
-async def browser(pytestconfig):
-    async with async_playwright() as pw:
-        yield await pw.chromium.launch(headless= not pytestconfig.getoption('--headed'))
+async def display(server, page):
+    async with DisplayFixture(server, page) as display:
+        yield display
+
+
+@pytest.fixture(scope="session")
+async def server():
+    async with BackendFixture() as server:
+        yield server
 
 
 @pytest.fixture(scope="session")
@@ -33,12 +41,9 @@ async def page(browser):
     finally:
         await pg.close()
 
-@pytest.fixture(scope="session")
-async def server():
-    async with BackendFixture() as server:
-        yield server
 
 @pytest.fixture(scope="session")
-async def display(server, page):
-    async with DisplayFixture(server, page) as display:
-        yield display
+async def browser(pytestconfig):
+    async with async_playwright() as pw:
+        yield await pw.chromium.launch(headless = not pytestconfig.getoption('--headed'))
+
