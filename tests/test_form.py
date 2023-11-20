c0ec407da1d6b07649da99b520510019bf31dbc5
Steve Jones <jonesst2608@gmail.com>
2023-11-20 16:01:52 +0000

    Fixed erratic tests

--- tests/test_form.py
+++ tests/test_form.py
@@ -4,6 +4,19 @@ from reactpy.testing import DisplayFixture
 
 from examples.form_login import AppMain
 
+class Selector:
+
+    @property
+    async def selector(self):
+        await self._page.wait_for_load_state("networkidle")
+        element = await self._page.wait_for_selector(self._selector)
+        return element
+
+    def __init__(self, display, sel):
+        self._page = display.page
+        self._selector = sel
+
+
 # pytest -o log_cli=1 -sv tests/test_form.py
 
 @pytest.mark.anyio
@@ -12,24 +25,26 @@ async def test_form(display: DisplayFixture):
     await display.show(AppMain)
 
     async def wait_for_selector(selector):
+        await display.page.wait_for_load_state("networkidle")
         selector = await display.page.wait_for_selector(selector)
         return selector
-    
-    async def wait_for_input(selector):
-        selector = await wait_for_selector(selector)
-        value = await selector.input_value()
+
+    async def get_input(selector):
+        element = await wait_for_selector(selector)
+        value = await element.input_value()
         return value
 
-    email_input = await wait_for_selector("#email")
-    await email_input.fill('bigjoe@gmail.com')
+    async def set_input(selector, value):
+        element = await wait_for_selector(selector)
+        await element.fill(value)
 
-    email_input = await wait_for_selector("#email")
-    assert (await email_input.input_value()) == 'bigjoe@gmail.com'
+    assert (await get_input("#email")) == 'joe@gmail.com'
+
+    await set_input("#email", 'bigjoe@gmail.com')
+    assert (await get_input("#email")) == 'bigjoe@gmail.com'
+
+    await set_input("#email", 'xxx')
+    assert (await get_input("#email")) == 'xxx'
 
 
-    email_input = await wait_for_selector("#email")
-    await email_input.fill('xxx')
-
-    email_input = await wait_for_selector("#email")
-    assert (await email_input.input_value()) == 'xxx'
 
