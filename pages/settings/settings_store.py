785ca206e7070e362ef53bab2e1a6c1ee4b98e52
Steve Jones <jonesst2608@gmail.com>
2023-12-11 10:31:03 +0000

    Tidy up

--- pages/settings/settings_store.py
+++ pages/settings/settings_store.py
@@ -1,8 +1,7 @@
 from typing import cast
 from pydantic import BaseModel
-from reactpy import use_state, create_context
+from reactpy import create_context
 
-from utils.logger import log
 
 
 class UserSettings(BaseModel):
@@ -13,18 +12,15 @@ class UserState:
 
     @property
     def dark_mode(self) -> bool:
-        log.info('dark_mode=%s', self._settings.dark_mode)
         return self._settings.dark_mode
 
     def __init__(self, settings, set_settings):
-        log.info('UserState(settings [%s])', settings)
         self._settings = settings
         self._set_settings = set_settings
 
 
     def update(self, **kwargs):
         new = self._settings.copy(update=kwargs)
-        log.info('update new = [%s]', new)
         self._set_settings(new)
 
 
@@ -33,9 +29,3 @@ class UserState:
 
 
 SettingsContext = create_context(cast(UserState, None))
-
-# ./solidjs-dashboard/src/settings/settings.ts
-
-
-
-# settings, set_settings = use_state(UserSettings())
