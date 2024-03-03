6e0f03beefe814773ae74862e9a9ada4b26213f5
Steve Jones <jonesst2608@gmail.com>
2024-03-03 19:04:31 +0000

    Fix DarkModeButton

--- pages/components/dark_mode_button.py
+++ pages/components/dark_mode_button.py
@@ -1,7 +1,8 @@
-from reactpy import component, html, use_context
+from reactpy import component, html, use_context, event
 
 from .icon import Icon_Sun, Icon_Moon
 from .app_store import AppContext
+from utils.types import EventArgs
 
 @component
 def DarkModeButton():
@@ -9,10 +10,14 @@ def DarkModeButton():
 
     icon =  Icon_Sun() if settings.dark_mode else Icon_Moon()
 
+    @event
+    def onclick(event: EventArgs):
+        settings.toggle_dark_mode()
+
     return html.button({
         'id': 'theme-toggle',
         'type': 'button', 
         'class_name': 'rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none',
-        'onclick': lambda : settings.toggle_dark_mode()},
+        'onclick': onclick}, # pylint: disable=unnecessary-lambda
         icon,
     )
